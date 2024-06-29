3b86758047af88d1fb71ec4575e9ba123268c43e
Shreya Garg <shreyag.garg72@gmail.com>
2024-06-30 00:03:18 +0530

    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -150,6 +150,10 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use('/api/videos', videoRoutes);
@@ -163,10 +167,6 @@ app.get('/api/courses', async (req, res) => {
   }
 });
 app.use('/api', loginRegRoutes);
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
 
 // Error handler middleware
 app.use(errorHandler);
