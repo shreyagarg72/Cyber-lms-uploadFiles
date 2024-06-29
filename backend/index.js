c6cfa45b7e5a7a438c7134361eb6d7d4aa6984ac
Shreya Garg <shreyag.garg72@gmail.com>
2024-06-30 00:14:45 +0530

    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -152,11 +152,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
