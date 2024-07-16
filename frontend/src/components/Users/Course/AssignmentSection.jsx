14d4df7066c63afa0146ec5c418f3262dd8ac2fb
tanyasinha2003 <tanyasinha2003@gmail.com>
2024-07-16 16:13:09 +0530

    minor error in assignment section solved

--- frontend/src/components/Users/Course/AssignmentSection.jsx
+++ frontend/src/components/Users/Course/AssignmentSection.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AssignmentSection = ({ assignments }) => {
   const [selectedOptions, setSelectedOptions] = useState({});
   const [showResult, setShowResult] = useState({});
-  
+
+  useEffect(() => {
+    const initialShowResult = assignments.reduce((acc, _, index) => {
+      acc[index] = false;
+      return acc;
+    }, {});
+    setShowResult(initialShowResult);
+  }, [assignments]);
+
   const handleOptionChange = (questionNo, selectedOption) => {
     setSelectedOptions(prevState => ({
       ...prevState,
       [questionNo]: selectedOption
     }));
-    
+
     setShowResult(prevState => ({
       ...prevState,
       [questionNo]: true
@@ -20,23 +28,23 @@ const AssignmentSection = ({ assignments }) => {
     <div>
       {assignments.map((question, index) => (
         <div key={index}>
-          <h4 className='font-bold mb-2'>{`Question ${index+1}: ${question.questionText}`}</h4>
+          <h4 className='font-bold mb-2'>{`Question ${index + 1}: ${question.questionText}`}</h4>
           {question.options.map((option, idx) => (
-            <div key={idx}  className="option-box radio-container">
+            <div key={idx} className="option-box radio-container">
               <input
                 type="radio"
                 id={`question-${index}-option-${idx}`}
                 name={`question-${index}`}
                 value={option}
-                checked={selectedOptions[question.questionNo] === option}
-                onChange={() => handleOptionChange(question.questionNo, option)}
+                checked={selectedOptions[index] === option}
+                onChange={() => handleOptionChange(index, option)}
               />
               <label htmlFor={`question-${index}-option-${idx}`}> {option}</label>
             </div>
           ))}
-          {showResult[question.questionNo] && (
+          {showResult[index] && (
             <div>
-              {selectedOptions[question.questionNo] === question.correctAnswer ? (
+              {selectedOptions[index] === question.correctAnswer ? (
                 <p style={{ color: 'green' }}>Correct!</p>
               ) : (
                 <p style={{ color: 'red' }}>Incorrect! The correct answer is {question.correctAnswer}.</p>
